ca72814611daa8d93009ed3f4043a80a8bd05cb5
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-08 00:16:24 +0600

    Add typography

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,8 +1,35 @@
+
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import Logo from "./../../../assets/logo.svg"
+import './header.css'
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <Navbar bg="navbar-wrap" expand="lg">
+            <Container className="navbar-contains">
+                <Navbar.Brand href="#home" className="navbar-band-img">
+                    <img src={Logo} className="d-inline-block align-top " alt="Car Doctor logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mx-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#services">Services</Nav.Link>
+                        <Nav.Link href="#blog">Blog</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#search">
+                            <i className="fas fa-search"></i>
+                        </Nav.Link>
+                        <Nav.Link href="#cart">
+                            <i className="fas fa-shopping-cart"></i>
+                        </Nav.Link>
+                        <Button variant="outline-danger" href="#appointment">Appointment</Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 import {
   RouterProvider
 } from "react-router-dom";
 import router from './Routes/Routes.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <div className='font-family'>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  </div>
 )
