c943d6eedb27b1500ad0d244b4edff77022bfae1
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-08 00:41:36 +0600

    Header complete with responsive

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -4,7 +4,7 @@ import Logo from "./../../../assets/logo.svg"
 import './header.css'
 const Header = () => {
     return (
-        <Navbar bg="navbar-wrap" expand="lg">
+        <Navbar bg="navbar-wrap fs-18 fw-600 text-center" expand="lg">
             <Container className="navbar-contains">
                 <Navbar.Brand href="#home" className="navbar-band-img">
                     <img src={Logo} className="d-inline-block align-top " alt="Car Doctor logo" />
@@ -12,10 +12,10 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mx-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#services">Services</Nav.Link>
-                        <Nav.Link href="#blog">Blog</Nav.Link>
+                        <Nav.Link href="#home" className="mx-3">Home</Nav.Link>
+                        <Nav.Link href="#about" className="mx-3">About</Nav.Link>
+                        <Nav.Link href="#services" className="mx-3">Services</Nav.Link>
+                        <Nav.Link href="#blog" className="mx-3">Blog</Nav.Link>
                         <Nav.Link href="#contact">Contact</Nav.Link>
                     </Nav>
                     <Nav>
