f39b3cacb4b0b119f1f81ad296fdb603f947f6fb
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-08 14:03:22 +0600

    services section heading side complete

---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,16 @@
+import { Container } from "react-bootstrap";
+
+
+const Services = () => {
+    return (
+        <Container className="services-wrap">
+            <div className="services-contains">
+                <h3 className='fs-20 fw-700 font-orange mb-3 text-center'>About Us</h3>
+                <h2 className="fs-45 fw-700 font-dark-01 my-3 text-center">Our Service Area</h2>
+                <p className="fs-16 fw-400 line-height-30 text-capitalize text-center mb-3">the majority have suffered alteration in some form, by injected humour, or randomised <br />words which do not look even slightly believable. </p>
+            </div>
+        </Container>
+    );
+};
+
+export default Services;
