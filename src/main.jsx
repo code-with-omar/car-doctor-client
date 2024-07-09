9ce0ab6a33a613df4a37fac396f563f0c0224f83
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-09 20:40:08 +0600

    Create context API

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,13 @@ import {
   RouterProvider
 } from "react-router-dom";
 import router from './Routes/Routes.jsx';
+import AuthProvider from './Provider/AuthProvider.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='font-family'>
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </React.StrictMode>,
   </div>
 )
