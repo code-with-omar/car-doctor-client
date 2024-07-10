f8303ebd01d842cf25931c409acae2170c9d56a1
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-11 00:23:25 +0600

    Create handle form in checkout form

--- src/Pages/CheckOut/CheckOut.jsx
+++ src/Pages/CheckOut/CheckOut.jsx
@@ -1,11 +1,23 @@
 import { useLoaderData } from 'react-router-dom';
-import CheckOutImage from "./../../assets/images/checkout/checkout.png"
 import './checkOut.css'
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { Col, Container, Form, Row } from 'react-bootstrap';
+import { useContext } from 'react';
+import { AuthContext } from '../../Provider/AuthProvider';
+
 
 const CheckOut = () => {
-    const checkOutData = useLoaderData();
+    const checkOutData = useLoaderData()
     const { title, price } = checkOutData
+    const { user } = useContext(AuthContext)
+    const handleCheckOut=(e)=>{
+        e.preventDefault()
+        const form=e.target;
+        const name=form.name.value;
+        const email=user?.email|| form.email.value;
+        const price=user?.price|| form.price.value
+        console.log({name,email,price});
+        form.reset()
+    }
     return (
         <Container className='checkout-wrap'>
             <div className='checkout-banner-wrap'>
@@ -13,28 +25,28 @@ const CheckOut = () => {
                     <h2 className='checkout-header fs-45 fw-500 font-white'>Check Out</h2>
                 </div>
             </div>
-            <Form className='bg-dark-07 checkout-form'>
+            <Form className='bg-dark-07 checkout-form' onSubmit={handleCheckOut}>
                 <h2 className=' fs-35 fw-700 font-orange text-center mb-5'>{title}</h2>
                 <Row className="mb-3">
                     <Form.Group as={Col} controlId="formGridName">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" name="name" placeholder="Enter Name" />
+                        <Form.Control type="text" name="name" placeholder="Enter Name" defaultValue={user?.name} required/>
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridDate">
                         <Form.Label>Date</Form.Label>
-                        <Form.Control type="date" name="date" placeholder="Date" />
+                        <Form.Control type="date" name="date" placeholder="Date" required/>
                     </Form.Group>
                 </Row>
                 <Row className="mb-3">
                     <Form.Group as={Col} controlId="formGridName">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" name="email" placeholder="Enter Name" />
+                        <Form.Control type="email" name="email" placeholder="Enter Name" defaultValue={user?.email} required/>
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridDate">
                         <Form.Label>Price</Form.Label>
-                        <Form.Control type="text" name="price" placeholder="Price" defaultValue={`$ ${price}`}/>
+                        <Form.Control type="text" name="price" defaultValue={`$ ${price}`} disabled/>
                     </Form.Group>
                 </Row>
                 <Row>
