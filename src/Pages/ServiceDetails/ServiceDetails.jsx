e2e7dde0d3961f9b0c3a82ed2f4f9fd8338a2f0d
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-12 01:32:44 +0600

    Full complete the service details page

--- src/Pages/ServiceDetails/ServiceDetails.jsx
+++ src/Pages/ServiceDetails/ServiceDetails.jsx
@@ -19,6 +19,7 @@ const ServiceDetails = () => {
     }, []);
     console.log(facility)
     return (
+
         <Container className="service-details-wrap">
             <div className='checkout-banner-wrap'>
                 <div className='checkout-banner'>
@@ -77,36 +78,56 @@ const ServiceDetails = () => {
                         </Row>
                     </div>
                 </Col>
-                <Col lg={3}>
-                    {
-                        services.map(service => <h2 key={service._id}>{service.title}</h2>)
-                    }
-                    <div>
-                        <h3>Download</h3>
-                        <div>
-                            <p>Our Brochure</p>
-                            <small>Download</small>
-                            <FaArrowRight></FaArrowRight>
+                <Col lg={3} className="service-details-right ">
+                    <div className="bg-dark-07 p-5 rounded mb-5">
+                        <h3 className="fs-25 fw-700 font-dark-01 ">Services</h3>
+                        {
+                            services.map(service =>
+                                <div key={service._id} className="details-service">
+                                    <Link to={`/service/${service._id}`} className="d-block   mb-3 p-3 rounded fs-16 font-dark-01 fw-600" >{service.title}
+                                    </Link>
+                                </div>)
+                        }
+                    </div>
+                    <div className="download-section bg-dark-01 font-white rounded fw-600 p-5">
+                        <h3 className="fs-25">Download</h3>
+                        <div className="d-flex justify-content-between mb-5">
+                            <div>
+                                <p className="fs-16 m-0">Our Brochure</p>
+                                <small className="fs-14 font-dark-04 fw-400">Download</small>
+                            </div>
+                            <div className="bg-orange-01 px-3 py-3 rounded download-fav">
+                                <FaArrowRight className=""></FaArrowRight>
+                            </div>
                         </div>
-                        <div>
-                            <p>Company Details</p>
-                            <small>Download</small>
-                            <FaArrowRight></FaArrowRight>
+                        <div className="d-flex justify-content-between mb-3">
+                            <div>
+                                <p className="fs-16 m-0">Company Details</p>
+                                <small className="fs-14 font-dark-04 fw-400">Download</small>
+                            </div>
+                            <div className="bg-orange-01 px-3 py-3 rounded download-fav">
+                                <FaArrowRight className=""></FaArrowRight>
+                            </div>
                         </div>
                     </div>
-                    <div>
+                    <div className="bg-dark-01 p-5 text-center mt-5 rounded font-white fw-700 car-doctor-special">
                         <img src={logo} alt="" />
-                        <p>Need Help? We Are Here To Help You</p>
-                        <div>
-                            <h2>Car Doctor <span>Special</span></h2>
-                            <p>Save up to <span>60% off</span></p>
+                        <p className="my-5 fs-200 line-height-30 why-need">Need Help? We Are Here To Help You</p>
+                        <div className="bg-white rounded p-2 position-relative ">
+                            <h2 className="fs-20 fw-600 font-orange-01 line-height-30">Car Doctor <span className="font-dark-01 ">Special</span></h2>
+                            <p className="fs-16 fw-700">Save up to <span className="span">60% off</span></p>
+                            <div className="bg-orange-01 position-absolute car-special-btn rounded">
+                                <Button className="bg-orange-01 p-2">Get A Quote</Button>
+                            </div>
                         </div>
                     </div>
-                    <div>
-                        <h2>Price ${price}</h2>
-                        <Link to={`/order/${_id}`}>
-                            <Button> Proceed Checkout</Button>
-                        </Link>
+                    <div className="pt-5 price-checkout">
+                        <h2 className="fs-35 fw-700 mb-3">Price ${price}</h2>
+                        <div className="price-checkout">
+                            <Link to={`/order/${_id}`}>
+                                <Button variant="outline-info" size="lg"> Proceed Checkout</Button>
+                            </Link>
+                        </div>
                     </div>
                 </Col>
             </Row>
