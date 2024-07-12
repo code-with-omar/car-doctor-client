5a4de2c70a39ec86fdd0284fdd96b6986713f779
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-12 23:00:44 +0600

    Create logout route and added it in header

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -3,7 +3,17 @@ import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import Logo from "./../../../assets/logo.svg"
 import './header.css'
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../../Provider/AuthProvider";
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext)
+    const handleLogOut=()=>{
+        logOut()
+        .then(()=>{})
+        .catch(error=>{
+            console.log(error)
+        })
+    }
     return (
         <Navbar bg="navbar-wrap fs-18 fw-600 text-center" expand="lg">
             <Container className="navbar-contains">
@@ -16,7 +26,11 @@ const Header = () => {
                         <Link className="mx-4 font-dark-02 fs-18 fw-600" to='/'>Home</Link>
                         <Link className="mx-4 font-dark-02 fs-18 fw-600" to='/addServices'>Add Services</Link>
                         <Link className="mx-4 font-dark-02 fs-18 fw-600" to='/signUp'>Sign up</Link>
-                        <Link className="mx-4 font-dark-02 fs-18 fw-600" to='/signIn'>Sign In</Link>
+                       
+                        {
+                            user?.email ? <Link className="mx-4 font-dark-02 fs-18 fw-600" onClick={handleLogOut}>LogOut</Link> : <Link className="mx-4 font-dark-02 fs-18 fw-600" to='/signIn'>Log In</Link>
+                        }
+
                     </Nav>
                     <Nav>
                         <Nav.Link href="#search">
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth"
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth"
 import app from "../firebase/firebase.config";
 export const AuthContext = createContext()
 const auth = getAuth(app)
@@ -16,6 +16,11 @@ const AuthProvider = ({ children }) => {
         setLoading(true)
         return signInWithEmailAndPassword(auth,email,password)
     }
+    // logout 
+    const logOut=()=>{
+        setLoading(true)
+        return signOut(auth)
+    }
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
@@ -30,7 +35,8 @@ const AuthProvider = ({ children }) => {
         user,
         loading,
         createUser,
-        signIn
+        signIn,
+        logOut
     }
     return (
         <AuthContext.Provider value={authInfo}>
