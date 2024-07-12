1f6e4904578b884344b8e37a141668163de22c08
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-12 19:04:21 +0600

    Handle add service functionility complete

--- src/Pages/AddServices/AddServices.jsx
+++ src/Pages/AddServices/AddServices.jsx
@@ -1,6 +1,57 @@
 import { Col, Container, Form, Row } from 'react-bootstrap';
 import './addServices.css'
 const AddServices = () => {
+    const handleAddServices = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const title = form.name.value;
+        const service_id = form.serviceName.value;
+        const img = form.image.value;
+        const price = form.price.value;
+        const description = form.description.value;
+        const nameOfFacility_1 = form.nameOfFacility_1.value;
+        const nameOfFacility_2 = form.nameOfFacility_2.value;
+        const nameOfFacility_3 = form.nameOfFacility_3.value;
+        const nameOfFacility_4 = form.nameOfFacility_4.value;
+        const facilityDescription_1 = form.facilityDescription_1.value;
+        const facilityDescription_2 = form.facilityDescription_2.value;
+        const facilityDescription_3 = form.facilityDescription_3.value;
+        const facilityDescription_4 = form.facilityDescription_4.value;
+
+        const facility = [
+            {
+                service_id,
+                title,
+                img,
+                price,
+                description,
+                facility: [
+                    {
+                        name: nameOfFacility_1,
+                        details: facilityDescription_1
+                    },
+                    {
+                        name: nameOfFacility_2,
+                        details: facilityDescription_2
+                    },
+                    {
+                        name: nameOfFacility_3,
+                        details: facilityDescription_3
+                    },
+                    {
+                        name: nameOfFacility_4,
+                        details: facilityDescription_4
+                    }
+                ]
+            }
+        ];
+
+        console.log(facility);
+        
+    };
+
+
+
     return (
         <Container>
             <div className='checkout-banner-wrap'>
@@ -8,8 +59,7 @@ const AddServices = () => {
                     <h2 className='checkout-header fs-45 fw-500 font-white'>Service Details</h2>
                 </div>
             </div>
-            <Form className='bg-dark-07 checkout-form' >
-                <h2 className=' fs-35 fw-700 font-orange text-center mb-5'></h2>
+            <Form className='bg-dark-07 checkout-form' onSubmit={handleAddServices}>
                 <Row className="mb-3">
                     <Form.Group as={Col} controlId="formGridName">
                         <Form.Label>Service Title</Form.Label>
@@ -24,7 +74,7 @@ const AddServices = () => {
                 <Row className="mb-3">
                     <Form.Group as={Col} controlId="formGridName">
                         <Form.Label>Image</Form.Label>
-                        <Form.Control type="email" name="email" placeholder="Image URL" required />
+                        <Form.Control type="text" name="image" placeholder="Image URL" required />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridDate">
@@ -43,45 +93,45 @@ const AddServices = () => {
                     <h4>Facilities</h4>
                     <Form.Group as={Col} controlId="formGridName">
                         <Form.Label></Form.Label>
-                        <Form.Control type="text" name="nameOfFacility" placeholder="Name of facility" required />
+                        <Form.Control type="text" name="nameOfFacility_1" placeholder="Name of facility" required />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridDate">
                         <Form.Label></Form.Label>
-                        <Form.Control type="text" name="facilityDescription" placeholder='Description' />
+                        <Form.Control type="text" name="facilityDescription_1" placeholder='Description' />
                     </Form.Group>
                 </Row>
                 <Row className="">
                     <Form.Group as={Col} controlId="formGridName">
                         <Form.Label></Form.Label>
-                        <Form.Control type="text" name="nameOfFacility" placeholder="Name of facility" required />
+                        <Form.Control type="text" name="nameOfFacility_2" placeholder="Name of facility" required />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridDate">
                         <Form.Label></Form.Label>
-                        <Form.Control type="text" name="facilityDescription" placeholder='Description' />
+                        <Form.Control type="text" name="facilityDescription_2" placeholder='Description' />
                     </Form.Group>
                 </Row>
                 <Row className="">
                     <Form.Group as={Col} controlId="formGridName">
                         <Form.Label></Form.Label>
-                        <Form.Control type="text" name="nameOfFacility" placeholder="Name of facility" required />
+                        <Form.Control type="text" name="nameOfFacility_3" placeholder="Name of facility" required />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridDate">
                         <Form.Label></Form.Label>
-                        <Form.Control type="text" name="facilityDescription" placeholder='Description' />
+                        <Form.Control type="text" name="facilityDescription_3" placeholder='Description' />
                     </Form.Group>
                 </Row>
                 <Row className="">
                     <Form.Group as={Col} controlId="formGridName">
                         <Form.Label></Form.Label>
-                        <Form.Control type="text" name="nameOfFacility" placeholder="Name of facility" required />
+                        <Form.Control type="text" name="nameOfFacility_4" placeholder="Name of facility" required />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridDate">
                         <Form.Label></Form.Label>
-                        <Form.Control type="text" name="facilityDescription" placeholder='Description' />
+                        <Form.Control type="text" name="facilityDescription_4" placeholder='Description' />
                     </Form.Group>
                 </Row>
                 <Row>
