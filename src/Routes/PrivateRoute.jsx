b3bac3587078e8debaa273deef9e1e86631318b6
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-16 12:20:56 +0600

    Add spinner in private route

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,15 +1,18 @@
 import { useContext } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
-import { Spinner } from "react-bootstrap";
+import { Container, Spinner } from "react-bootstrap";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
     if (loading) {
         return (
-            <Spinner animation="border" role="status">
-                <span className="visually-hidden">Loading...</span>
-            </Spinner>
+            <Container className="w-25 m-auto my-5">
+                <Spinner animation="grow" role="status" variant="success"></Spinner>
+                <Spinner animation="grow" variant="success" className="mx-2"/>
+                <Spinner animation="grow" variant="danger" className="mx-2"/>
+                <Spinner animation="grow" variant="warning" className="mx-2"/>
+            </Container>
         )
     }
     if (user?.email) {
