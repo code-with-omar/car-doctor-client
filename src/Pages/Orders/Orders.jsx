7bd9799885e52fe88caaa57fd6704f03c812342c
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-17 17:02:51 +0600

    create order all function and order go to the database

--- src/Pages/Orders/Orders.jsx
+++ src/Pages/Orders/Orders.jsx
@@ -2,11 +2,12 @@ import { useContext } from "react";
 import { Col, Container, Row, Form } from "react-bootstrap";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider";
+import Swal from "sweetalert2";
 
 const Orders = () => {
     const product = useLoaderData();
     const { user } = useContext(AuthContext);
-    const { title, price } = product;
+    const { title, price, image } = product;
 
     const handleOrder = (e) => {
         e.preventDefault();
@@ -14,7 +15,36 @@ const Orders = () => {
         const name = form.name.value;
         const place = form.place.value
         const email = form.email.value;
-        const price = user?.price || form.price.value
+        const price = user?.price || form.price.value;
+        const order = {
+            customerName: name,
+            email,
+            place,
+            price,
+            image
+        }
+        fetch(`http://localhost:5000/productsOrder`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    Swal.fire({
+                        position: "top",
+                        icon: "success",
+                        title: "Booking Success",
+                        showConfirmButton: true,
+                        confirmButtonText: "Close",
+                        timer: 1500
+                    });
+                }
+
+            })
+        form.reset()
     };
 
     return (
@@ -24,7 +54,7 @@ const Orders = () => {
                     <h2 className="checkout-header fs-45 fw-500 font-white">Order Product</h2>
                 </div>
             </div>
-            <Form className="bg-dark-07 orders-form" onSubmit={handleOrder}>
+            <Form className="bg-dark-07 orders-form p-5" onSubmit={handleOrder}>
                 <h2 className="fs-35 fw-700 font-orange text-center mb-5">{title}</h2>
 
                 <Row className="mb-3">
